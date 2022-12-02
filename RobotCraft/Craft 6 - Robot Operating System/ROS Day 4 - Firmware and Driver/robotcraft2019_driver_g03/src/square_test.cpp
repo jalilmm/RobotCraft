dce6c9bc21c1823f9d8267465b82ed6f7f235da4
Jalil Mahmud <53440786+jalilmm@users.noreply.github.com>
2022-12-02 09:42:30 +0200

    Update square_test.cpp

--- RobotCraft/Craft 6 - Robot Operating System/ROS Day 4 - Firmware and Driver/robotcraft2019_driver_g03/src/square_test.cpp
+++ RobotCraft/Craft 6 - Robot Operating System/ROS Day 4 - Firmware and Driver/robotcraft2019_driver_g03/src/square_test.cpp
@@ -1,13 +1,4 @@
-/**
- * @file square_test.cpp
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2019-08-17
- * 
- * @copyright Copyright (c) 2019
- * 
- */
+
 
 #include "square_controller.h"
 
